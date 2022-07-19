3d342c99f74a30ef661b10c9484b79b7b90ff561
Jourdan <jlambert@foggyclouds.io>
2022-07-18 21:53:17 -0400

    updates
    Signed-off-by: Jourdan <jlambert@foggyclouds.io>

--- docker-nfs-config.sh
+++ docker-nfs-config.sh
@@ -7,7 +7,7 @@ sudo apt-get update && sudo apt-get install -y nfs-common
 sudo mkdir -p /mnt/nfs_vol_01
 
 sudo echo '10.1.100.16:/volume1/docker    /mnt/nfs_vol_01   nfs    defaults    0    0' \
-    | sudo tee -a etc/fstab
+    | sudo tee -a /etc/fstab
 
 sudo mount 10.1.100.16:/volume1/docker /mnt/nfs_vol_01
 
