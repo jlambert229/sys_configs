723352b6f363d697db5f86497e95e30283d6f29b
Jourdan <jlambert@foggyclouds.io>
2022-07-18 22:05:15 -0400

    updates
    Signed-off-by: Jourdan <jlambert@foggyclouds.io>

---
+++ colors-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOCAL_USER=$1
+
+# configures console colors to one dark theme
+
+sudo cp ./colors/bashrc /home/"$LOCAL_USER"/.bashrc
+sudo cp ./colors/dircolors /home/"$LOCAL_USER"/.dircolors
+sudo cp ./colors/onedark_prompt.sh /home/"$LOCAL_USER"/.onedark_prompt.sh
+
+echp 'color changes completed, please log out and log back in.'
